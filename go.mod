147663f06d126fa12baabe0f209afe0c78d4bae4
Xue Jiao <jiao.xue@shopee.com>
2020-04-22 23:24:33 +0800

    Test more for IList

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/caser789/nstack
 
 go 1.13
+
+require github.com/mattn/goveralls v0.0.5 // indirect
--- ilist/interface_list_test.go
+++ ilist/interface_list_test.go
@@ -4,30 +4,43 @@ import (
     "testing"
 )
 
-func TestIList(t *testing.T) {
+func TestResetEmpty(t *testing.T) {
     lst := &List{}
-
-    if want, got := true, lst.Empty(); want != got {
-        t.Fatalf("lst not empty:\n- want: %v\n- got: %v", want, got)
-    }
-
-    e := &Entry{}
-    lst.PushFront(e)
+    a := &Entry{}
+    lst.PushFront(a)
 
     if want, got := false, lst.Empty(); want != got {
-        t.Fatalf("lst empty:\n- want: %v\n- got: %v", want, got)
+        t.Fatalf("TestReset lst not empty:\n- want: %v\n- got: %v", want, got)
     }
+
+    lst.Reset()
+    if want, got := true, lst.Empty(); want != got {
+        t.Fatalf("TestReset lst empty:\n- want: %v\n- got: %v", want, got)
+    }
+}
+
+func TestFrontPushFront(t *testing.T) {
+    lst := &List{}
+    e := &Entry{}
+    lst.PushFront(e)
 
     if want, got := lst.Front(), e; want != got {
         t.Fatalf("front error:\n- want: %v\n- got: %v", want, got)
     }
+}
 
+func TestBackPushBack(t *testing.T) {
+    lst := &List{}
     b := &Entry{}
     lst.PushBack(b)
 
     if want, got := lst.Back(), b; want != got {
         t.Fatalf("back error:\n- want: %v\n- got: %v", want, got)
     }
+}
+
+func TestPushBackList(t *testing.T) {
+    lst := &List{}
 
     lst2 := &List{}
     c := &Entry{}
@@ -38,5 +51,39 @@ func TestIList(t *testing.T) {
 
     if want, got := lst.Back(), d; want != got {
         t.Fatalf("PushBackList error:\n- want: %v\n- got: %v", want, got)
+    }
+}
+
+func TestInsertAfter(t *testing.T) {
+    lst := &List{}
+
+    a := &Entry{}
+    b := &Entry{}
+    c := &Entry{}
+    lst.PushBack(a)
+    lst.PushBack(c)
+
+    lst.InsertAfter(a, b)
+    lst.Remove(c)
+
+    if want, got := lst.Back(), b; want != got {
+        t.Fatalf("TestInsertAfter error:\n- want: %v\n- got: %v", want, got)
+    }
+}
+
+func TestInsertBefore(t *testing.T) {
+    lst := &List{}
+
+    a := &Entry{}
+    b := &Entry{}
+    c := &Entry{}
+    lst.PushBack(a)
+    lst.PushBack(c)
+
+    lst.InsertBefore(c, b)
+    lst.Remove(a)
+
+    if want, got := lst.Front(), b; want != got {
+        t.Fatalf("TestInsertBefore error:\n- want: %v\n- got: %v", want, got)
     }
 }
