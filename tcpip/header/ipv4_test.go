bda0c0f4fc578ac6cc3013aeda112bd3e555159d
Xue Jiao <jiao.xuejiao@gmail.com>
2020-04-26 14:57:32 +0800

    Add ipv4.HeaderLength

--- tcpip/header/ipv4_test.go
+++ tcpip/header/ipv4_test.go
@@ -1,30 +1,56 @@
 package header
 
 import (
-    "testing"
+	"testing"
 )
 
 func TestIPVersion(t *testing.T) {
-    var tests = []struct{
-        b []byte
-        expected int
-    }{
-        {
-            b: []byte{},
-            expected: -1,
-        },
-        {
-            b: []byte{byte(4<<4)},
-            expected: 4,
-        },
-    }
+	var tests = []struct {
+		b        []byte
+		version int
+	}{
+		{
+			b:        []byte{},
+			version: -1,
+		},
+		{
+			b:        []byte{byte(4 << 4)},
+			version: 4,
+		},
+	}
 
-    for _, test := range tests {
-        v := IPVersion(test.b)
+	for _, test := range tests {
+		v := IPVersion(test.b)
 
-        if want, got := test.expected, v; want != got {
-            t.Fatalf("TestIPVersion failed:\n- want: %v\n- got: %v", want, got)
-        }
-    }
+		if want, got := test.version, v; want != got {
+			t.Fatalf("TestIPVersion failed:\n- want: %v\n- got: %v", want, got)
+		}
+	}
 }
 
+
+func TestHeaderLength(t *testing.T) {
+    var tests = []struct{
+        b IPv4
+        length uint8
+    }{
+        {
+            b: IPv4([]byte{byte(5)}),
+            length: 20,
+        },
+        {
+            b: IPv4([]byte{byte(15)}),
+            length: 60,
+        },
+    }
+
+	for _, test := range tests {
+
+		v := test.b.HeaderLength()
+
+		if want, got := test.length, v; want != got {
+			t.Fatalf("TestHeaderLength failed:\n- want: %v\n- got: %v", want, got)
+		}
+	}
+
+}
